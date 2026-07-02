43a60684e06b056884bda090afedf491bdc35db8
dyeegoo <43637713+dyeegoo@users.noreply.github.com>
2026-07-02 19:07:21 +0330

    Update dependencies in build.gradle.kts
    Updated dependencies to latest versions for core-ktx, lifecycle-runtime-ktx, activity-compose, navigation-compose, hilt-navigation-compose, and firebase-bom. Added comments in Persian regarding updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,24 +45,33 @@ android {
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    
+    // آپدیت کردن BOM به نسخه جدید برای حل ارور SwipeToDismissBox و توابع جدید
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    
+    // آپدیت نویگیشن برای حل ارورهای navArgument و NavType
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    
+    // آپدیت فایربیس
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx")
+    
     implementation("com.google.ai.client.generativeai:generativeai:0.2.2")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
